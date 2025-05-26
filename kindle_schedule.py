fa366f8557ccbfbe97692eb4888b0a7111e3249c
dogeystamp@disroot.org <dogeystamp@disroot.org>
2025-05-25 21:04:22 -0400

    build: licence added

--- kindle_schedule.py
+++ kindle_schedule.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env -S uv run --script
 
+# This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 3 of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with this program. If not, see https://www.gnu.org/licenses/.
+#
+# © 2025 dogeystamp <dogeystamp@disroot.org>
+
 # /// script
 # requires-python = ">=3.11"
 # dependencies = [
@@ -8,7 +16,6 @@
 #     "typst==0.13.2",
 # ]
 # ///
-
 
 import dataclasses
 import functools
